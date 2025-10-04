65f1d1d4a1f7e607a501b52efdb4ac725d8908ed
David Chen <chen7david@me.com>
2025-10-04 13:34:02 -0400

    chore: setup basic router

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 
 
@@ -11,9 +10,7 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
 )
